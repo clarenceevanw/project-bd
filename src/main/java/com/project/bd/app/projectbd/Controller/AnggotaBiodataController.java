0e2cd6abd4569aee9e3bff9ef8c18749e38ffad2
ezrabs <ezra.bryan2006@gmail.com>
2025-05-29 01:37:56 +0700

    EDIT ANGGOTA DASHBOARD

---
+++ src/main/java/com/project/bd/app/projectbd/Controller/AnggotaBiodataController.java
@@ -0,0 +1,16 @@
+package com.project.bd.app.projectbd.Controller;
+
+import javafx.fxml.FXML;
+
+public class AnggotaBiodataController extends BaseController {
+
+//    @FXML
+//    private Label nrp;
+//    @FXML
+//    private Label nama;
+//    @FXML
+//    private Label email;
+//    @FXML
+//    private Label Program;
+
+}
--- src/main/java/com/project/bd/app/projectbd/Controller/AnggotaDashboardController.java
+++ src/main/java/com/project/bd/app/projectbd/Controller/AnggotaDashboardController.java
@@ -1,4 +1,21 @@
 package com.project.bd.app.projectbd.Controller;
 
+import com.project.bd.app.projectbd.Session.LoginSession;
+import com.project.bd.app.projectbd.utils.AlertNotification;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class AnggotaDashboardController extends BaseController {
+    @FXML
+    public void handleLogout() throws Exception {
+        LoginSession.getInstance().clearSession();
+        switchScenes("login.fxml", "Login");
+        AlertNotification.showSuccess("Logout berhasil!");
+    }
+
+    @FXML
+    public void goToBiodata() throws IOException {
+
+    }
 }
