8702fd17d1fb82478c7759f53adfb4fb7cbd9eb6
ezrabs <ezra.bryan2006@gmail.com>
2025-05-29 21:19:21 +0700

    FEAT: Biodata

--- src/main/java/com/project/bd/app/projectbd/Controller/AnggotaBiodataController.java
+++ src/main/java/com/project/bd/app/projectbd/Controller/AnggotaBiodataController.java
@@ -1,16 +1,59 @@
 package com.project.bd.app.projectbd.Controller;
 
+import com.project.bd.app.projectbd.DAO.MahasiswaDAO;
+import com.project.bd.app.projectbd.Model.Mahasiswa;
+import com.project.bd.app.projectbd.Session.LoginSession;
+import com.project.bd.app.projectbd.utils.AlertNotification;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.util.UUID;
 
 public class AnggotaBiodataController extends BaseController {
 
-//    @FXML
-//    private Label nrp;
-//    @FXML
-//    private Label nama;
-//    @FXML
-//    private Label email;
-//    @FXML
-//    private Label Program;
+    @FXML
+    private Label nrpLabel;
 
+    @FXML
+    private Label namaLabel;
+
+    @FXML
+    private Label programLabel;
+
+    @FXML
+    private Label prodiLabel;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private VBox sidebarAnggotaBiodata;
+
+    @FXML
+    public void initialize() {
+        // Posisi awal di luar layar (geser ke kiri)
+        sidebarAnggotaBiodata.setTranslateX(-300);
+
+        // Animasi slide in dari kiri
+        TranslateTransition slideIn = new TranslateTransition(Duration.millis(1000), sidebarAnggotaBiodata);
+        slideIn.setToX(0);
+        slideIn.play();
+    }
+
+    private void showBiodata() throws Exception {
+        // Ambil data dari session login
+        UUID idMahasiswa = LoginSession.getInstance().getIdMahasiswa();
+        MahasiswaDAO mahasiswaDAO = new MahasiswaDAO();
+        Mahasiswa mahasiswa = mahasiswaDAO.findById(idMahasiswa);
+        if(mahasiswa != null){
+            nrpLabel.setText(mahasiswa.getNrp());
+            namaLabel.setText(mahasiswa.getNama());
+//            programLabel.setText(mahasiswa.getIdProgram());
+//            prodiLabel.setText(mahasiswa.getIdProdi());
+            emailLabel.setText(mahasiswa.getEmail());
+        }
+    }
 }
--- src/main/java/com/project/bd/app/projectbd/Controller/AnggotaDashboardController.java
+++ src/main/java/com/project/bd/app/projectbd/Controller/AnggotaDashboardController.java
@@ -2,11 +2,27 @@ package com.project.bd.app.projectbd.Controller;
 
 import com.project.bd.app.projectbd.Session.LoginSession;
 import com.project.bd.app.projectbd.utils.AlertNotification;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class AnggotaDashboardController extends BaseController {
+    @FXML
+    private VBox sidebarDashboardAnggota;
+
+    @FXML
+    public void initialize() {
+        // Posisi awal di luar layar (geser ke kiri)
+        sidebarDashboardAnggota.setTranslateX(-300);
+
+        // Animasi slide in dari kiri
+        TranslateTransition slideIn = new TranslateTransition(Duration.millis(1000), sidebarDashboardAnggota);
+        slideIn.setToX(0);
+        slideIn.play();
+    }
     @FXML
     public void handleLogout() throws Exception {
         LoginSession.getInstance().clearSession();
@@ -16,6 +32,6 @@ public class AnggotaDashboardController extends BaseController {
 
     @FXML
     public void goToBiodata() throws IOException {
-
+        switchScenes("anggota/biodata.fxml", "Biodata Anggota");
     }
 }
