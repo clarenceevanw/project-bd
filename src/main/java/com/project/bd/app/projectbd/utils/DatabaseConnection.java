3a470e5263c9221c7b2f9c74c528b4ff31b5de54
ezrabs <ezra.bryan2006@gmail.com>
2025-06-03 20:16:40 +0700

    03 06 2025 DETAIL LIST CLUB FIXXX

--- src/main/java/com/project/bd/app/projectbd/utils/DatabaseConnection.java
+++ src/main/java/com/project/bd/app/projectbd/utils/DatabaseConnection.java
@@ -22,7 +22,7 @@ public class DatabaseConnection {
         // Kalau belum ada koneksi, buat baru
         String databaseName = "Project-BD";
         String user = "postgres";
-        String password = "abcd1234";
+        String password = "santoso135";
         String url = "jdbc:postgresql://localhost:5432/" + databaseName;
 
         try {
